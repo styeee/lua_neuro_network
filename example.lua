fdd2b861cc31992e8464933024c0f3c1bab41fb7
styeee <91516623+styeee@users.noreply.github.com>
2023-06-05 10:36:17 +0700

    Update example.lua
    ITS WORK!

--- example.lua
+++ example.lua
@@ -1,4 +1,4 @@
-file=io.open([[C:\Users\learn\Desktop\map.txt]])
+file=io.open([[map.txt]])
 io.input(file)
 i=1
 map={}
@@ -22,19 +22,19 @@ end
 
 require("neo")
 
-neo=NeuralNetwork.create(2,1,4,4,.3)
+net=neo.create(2,1,4,4,.3)
 ---[[
 i=0
 while true do i=i+1
-neo:backwardPropagate(A[1],{1})
-neo:backwardPropagate(A[2],{1})
-neo:backwardPropagate(A[3],{1})
-neo:backwardPropagate(A[4],{1})
+net:learn(A[1],{1})
+net:learn(A[2],{1})
+net:learn(A[3],{1})
+net:learn(A[4],{1})
 
-neo:backwardPropagate(B[1],{0})
-neo:backwardPropagate(B[2],{0})
-neo:backwardPropagate(B[3],{0})
-neo:backwardPropagate(B[4],{0})
+net:learn(B[1],{0})
+net:learn(B[2],{0})
+net:learn(B[3],{0})
+net:learn(B[4],{0})
 
 if i%1000==0 then
 
@@ -47,7 +47,7 @@ for x=1,#map[1] do
 temp=" "
 if string.sub(map[y],x,x)=="B" then temp="B " 
 elseif string.sub(map[y],x,x)=="A" then temp="A " 
-else temp=math.floor(tostring(neo:forewardPropagate(x,y)[1])+.5)..temp end
+else temp=math.floor((net:how(x,y)[1])+.5)..temp end
 text=text..temp
 end
 print(text)
